bab0fee78154241a7635f71c73c72402487b62d5
Lev <levberezov01@gmail.com>
2023-01-25 16:01:50 +0200

    second commit

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,21 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 public class Main {
     public static void main(String[] args) {
+        User user = new User();
+        user.setName("Vladimir Zelensky");
+        user.setAge(45);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(user);
+        userService.getAll();
+
 
     }
 }
